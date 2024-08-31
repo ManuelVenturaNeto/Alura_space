c4907fdb34a81bebf96eae3950867dacf1f39f4f
ManuelVenturaNeto <manuel.ventura.neto@outlook.com>
2024-08-31 15:55:17 -0300

    alura space

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin # type: ignore
 from django.urls import path, include # type: ignore
+from django.conf import settings # type: ignore
+from django.conf.urls.static import static # type: ignore
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('galeria.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
