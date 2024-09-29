c84f113b3d0e1dd8bcdfd3c0fa94e2c630355029
ManuelVenturaNeto <manuel.ventura.neto@outlook.com>
2024-09-29 17:39:16 -0300

    alura space sem api

--- galeria/models.py
+++ galeria/models.py
@@ -1,5 +1,6 @@
 from django.db import models # type: ignore
 from datetime import datetime
+from django.contrib.auth.models import User
 
 class Fotografia(models.Model):
     
@@ -10,8 +11,6 @@ class Fotografia(models.Model):
         ("PLANETA", "Planeta"),
     ]
     
-    
-    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default=' ')
@@ -20,6 +19,13 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to="foto/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete= models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="user"
+    )
     
     def __str__(self):
         return self.nome
